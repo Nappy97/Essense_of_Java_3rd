970d4d38031d3f656450829e4c0908777a275187
nappy <wjddmlgud829@gmail.com>
2021-06-28 21:45:41 +0900

    자바의 정석 3편

---
+++ src/com/nappy/Book1/Chapter3/OperatorEx17.java
@@ -0,0 +1,10 @@
+package com.nappy.Book1.Chapter3;
+
+public class OperatorEx17 {
+    public static void main(String[] args) {
+        double pi = 3.141592;
+        double shortPi = (int)(pi * 1000 + 0.5) / 1000.0;
+
+        System.out.println(shortPi);
+    }
+}
