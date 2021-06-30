88a675ee4923c5aed2ff40ba23f6d27b423dc3f0
nappy <wjddmlgud829@gmail.com>
2021-06-30 17:44:04 +0900

    자바의 정석 3편

---
+++ src/com/nappy/Book1/Chapter4/FlowEx23.java
@@ -0,0 +1,11 @@
+package com.nappy.Book1.Chapter4;
+
+public class FlowEx23 {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while (i-- != 0) {
+            System.out.println(i + " - I can do it");
+        }
+    }
+}
